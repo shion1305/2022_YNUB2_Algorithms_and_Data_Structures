d496f124a97e26940dc361b32a05dad9e86c65b0
Shion Ichikawa <shion1305@gmail.com>
2022-06-02 14:04:35 +0900

    [UPDATE] commented out unused functions in Lesson7_2164027.c

--- Lesson7_2164027.c
+++ Lesson7_2164027.c
@@ -71,48 +71,48 @@ void print_statistics_by_age_gender() {
     //クイックソートを用いて揃える。
 }
 
-//値を交換する関数
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-/*クイックソート*/
-void QuickSort(int A[], int left, int right){
-    // 変数定義
-    int Left, Right;
-    int pivot;
-
-    // 初期値は引数から
-    Left = left; Right = right;
-
-    // 基準は真ん中に設定
-    pivot = A[(left + right) / 2];
-
-    // ソーティング
-    while (1) {
-        // 基準より小さい値を左から見つけていく
-        while (A[Left] < pivot) Left++;
-
-        // 基準より大きい値を右から見つけていく
-        while (pivot < A[Right]) Right--;
-
-        // 見つかった値の順序が逆になったら終了
-        if (Left >= Right) break;
-
-        // 値を交換
-        swap(&A[Left], &A[Right]);
-
-        // 次の値に移動
-        Left++; Right--;
-
-    }
-
-    //左のデータ群を対象としてクイックソートを再帰
-    if (left < Left - 1) QuickSort(A, left, Left - 1);
-
-    //右のデータ群を対象としてクイックソートを再帰
-    if (Right + 1 < right) QuickSort(A, Right + 1, right);
-
-}
+////値を交換する関数
+//void swap(int *a, int *b) {
+//    int temp = *a;
+//    *a = *b;
+//    *b = temp;
+//}
+//
+///*クイックソート*/
+//void QuickSort(int A[], int left, int right){
+//    // 変数定義
+//    int Left, Right;
+//    int pivot;
+//
+//    // 初期値は引数から
+//    Left = left; Right = right;
+//
+//    // 基準は真ん中に設定
+//    pivot = A[(left + right) / 2];
+//
+//    // ソーティング
+//    while (1) {
+//        // 基準より小さい値を左から見つけていく
+//        while (A[Left] < pivot) Left++;
+//
+//        // 基準より大きい値を右から見つけていく
+//        while (pivot < A[Right]) Right--;
+//
+//        // 見つかった値の順序が逆になったら終了
+//        if (Left >= Right) break;
+//
+//        // 値を交換
+//        swap(&A[Left], &A[Right]);
+//
+//        // 次の値に移動
+//        Left++; Right--;
+//
+//    }
+//
+//    //左のデータ群を対象としてクイックソートを再帰
+//    if (left < Left - 1) QuickSort(A, left, Left - 1);
+//
+//    //右のデータ群を対象としてクイックソートを再帰
+//    if (Right + 1 < right) QuickSort(A, Right + 1, right);
+//
+//}
