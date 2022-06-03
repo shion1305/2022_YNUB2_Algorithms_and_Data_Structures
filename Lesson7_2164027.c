d00bceffa7041c64e39c3a106ff1e0e98f770a85
Shion Ichikawa <shion1305@gmail.com>
2022-06-03 11:41:33 +0900

    [UPDATE] final version of Lesson7_2164027.c

--- Lesson7_2164027.c
+++ Lesson7_2164027.c
@@ -19,6 +19,8 @@ typedef struct {
 
 void print_statistics_by_age_gender();
 
+void sort(int l, int r);
+
 int read();
 
 patient data[10000];
@@ -42,6 +44,7 @@ int read() {
 //〇〇年代、男/女で集計していく
 //今回のデータセットでは年代は10,20,20,30,40,50,60,70,80,90のいずれか。
 int size_by_category[2][9];
+int sorted[18];
 
 void print_statistics_by_age_gender() {
     for (int i = 0; i < 18; ++i) {
@@ -69,50 +72,70 @@ void print_statistics_by_age_gender() {
     }
     printf("+=====+========+======+\n");
     //クイックソートを用いて揃える。
+    //まずは初期化。インデックス番号の列を並び替えることでソートを行う。
+    for (int i = 0; i < 18; ++i) {
+        sorted[i] = i;
+    }
+
+    sort(0, 17);
+
+    //print header
+    printf("\n\n年齢と性別によるデータ統計\n");
+    printf("+=====+========+======+\n");
+    printf("| age | gender | size |\n");
+    printf("+=====+========+======+\n");
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            printf("|  %d0 |  ", i + 1);
+            if (j == 0) {
+                printf("  man");
+            } else {
+                printf("woman");
+            }
+            printf("   %3d |\n", size_by_category[j][i]);
+        }
+    }
+    printf("+=====+========+======+\n");
 }
 
-////値を交換する関数
-//void swap(int *a, int *b) {
-//    int temp = *a;
-//    *a = *b;
-//    *b = temp;
-//}
-//
-///*クイックソート*/
-//void QuickSort(int A[], int left, int right){
-//    // 変数定義
-//    int Left, Right;
-//    int pivot;
-//
-//    // 初期値は引数から
-//    Left = left; Right = right;
-//
-//    // 基準は真ん中に設定
-//    pivot = A[(left + right) / 2];
-//
-//    // ソーティング
-//    while (1) {
-//        // 基準より小さい値を左から見つけていく
-//        while (A[Left] < pivot) Left++;
-//
-//        // 基準より大きい値を右から見つけていく
-//        while (pivot < A[Right]) Right--;
-//
-//        // 見つかった値の順序が逆になったら終了
-//        if (Left >= Right) break;
-//
-//        // 値を交換
-//        swap(&A[Left], &A[Right]);
-//
-//        // 次の値に移動
-//        Left++; Right--;
-//
-//    }
-//
-//    //左のデータ群を対象としてクイックソートを再帰
-//    if (left < Left - 1) QuickSort(A, left, Left - 1);
-//
-//    //右のデータ群を対象としてクイックソートを再帰
-//    if (Right + 1 < right) QuickSort(A, Right + 1, right);
-//
-//}
+
+/*
+ * 今回の課題ではクイックソートを用いて
+ */
+//値を交換する関数
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int getValue(int i) {
+    return size_by_category[i % 2][i % 9];
+}
+
+void sort(int l, int r) {
+    //今回配列が属性の配列が18個あるので最初の値はl=0,r=17
+    // それぞれ変数を定義する
+    //基準値
+    int t;
+    //左右のポジションをメモ
+    int R, L;
+    //引数を元に初期値をセットする
+    R = r;
+    L = l;
+    // 基準は真ん中に設定。対応する値をgetValueで取得する。
+    t = getValue((l + r) / 2);
+    while (1) {
+        // 基準より大きい値を右から見つけていく
+        while (t < getValue(R)) R--;
+        //基準値と比較し、小さい値を見つけたら
+        while (getValue(L) < t) L++;
+        // LとRが反転した時に終了する。
+        if (L >= R) break;
+        // 並び替えの為のインデックス番号を格納した配列に対して置換処理を行う。
+        swap(&sorted[R--], &sorted[L++]);
+    }
+    //左右に分けたデータそれぞれに対して再帰処理
+    if (R + 1 < r) sort(R + 1, r);
+    if (l < L - 1) sort(l, L - 1);
+}
