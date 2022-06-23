76c2f0094a41ac65b2912a2f9267d7a8894c21c0
Shion Ichikawa <shion1305@gmail.com>
2022-06-24 00:03:12 +0900

    [CHORE] change in Lesson10_2164027.c

--- Lesson10_2164027.c
+++ Lesson10_2164027.c
@@ -43,7 +43,7 @@ int main() {
 }
 
 int checkAndMemo(int x, int y, int step, int data[N][N], int route[N][N]) {
-    if ((x < N && x >= 0) && (x < N && x >= 0)) {
+    if ((x < N && x >= 0) && (y < N && y >= 0)) {
         if ((route[x][y] == 0 || route[x][y] > step) && (data[x][y] == 0 || data[x][y] == -2)) {
             route[x][y] = step;
             return 1;
@@ -68,8 +68,57 @@ int countStep(int x, int y, int step, int data[N][N], int route[N][N]) {
     }
 }
 
+int trace_check(int x, int y, int route[N][N], int step) {
+    if ((x < N && x >= 0) && (y < N && y >= 0)) {
+        if (route[x][y] == step) return 1;
+    }
+    return 0;
+}
+
+int trace(int data[N][N], int route[N][N], int step, int x, int y) {
+    if (trace_check(x - 1, y, route, step - 1)) {
+
+        data[x - 1][y] = 1;
+        if (step > 1) {
+            trace(data, route, step - 1, x - 1, y);
+        }
+        return 0;
+    }
+
+    if (trace_check(x + 1, y, route, step - 1)) {
+        data[x + 1][y] = 1;
+        if (step > 1) {
+            trace(data, route, step - 1, x + 1, y);
+        }
+        return 0;
+    }
+
+    if (trace_check(x, y + 1, route, step - 1)) {
+        data[x][y + 1] = 1;
+        if (step > 1) {
+            trace(data, route, step - 1, x, y + 1);
+        }
+        return 0;
+    }
+
+    if (trace_check(x, y - 1, route, step - 1)) {
+        data[x][y - 1] = 1;
+        if (step > 1) {
+            trace(data, route, step - 1, x, y - 1);
+        }
+        return 0;
+    }
+}
+
 int process(int data[N][N], point start, point goal) {
     int route[N][N] = {{0,},};
     countStep(1, 1, 1, data, route);
     printf("%d", route[goal.x][goal.y]);
+    trace(data, route, route[goal.x][goal.y], goal.x, goal.y);
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            printf("%2d ", data[i][j]);
+        }
+        printf("\n");
+    }
 }
