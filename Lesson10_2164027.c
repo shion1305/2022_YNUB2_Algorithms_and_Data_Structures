46fec6a4c735cf7f2d5b98d86cc772fdbaeafecb
Shion Ichikawa <shion1305@gmail.com>
2022-06-24 00:15:00 +0900

    [CLEAN] code cleaning in Lesson10_2164027.c

--- Lesson10_2164027.c
+++ Lesson10_2164027.c
@@ -30,8 +30,7 @@ int map[N][N] = {
         {-1, 0,  0,  0,  -1, 0,  0,  0,  -2, -1},
         {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1},};
 typedef struct {
-    int x;
-    int y;
+    int x, y;
 } point;
 
 int process(int data[N][N], point start, point goal);
@@ -42,78 +41,47 @@ int main() {
     process(map, start, goal);
 }
 
-int checkAndMemo(int x, int y, int step, int data[N][N], int route[N][N]) {
-    if ((x < N && x >= 0) && (y < N && y >= 0)) {
-        if ((route[x][y] == 0 || route[x][y] > step) && (data[x][y] == 0 || data[x][y] == -2)) {
-            route[x][y] = step;
-            return 1;
-        }
-    }
-    return 0;
-}
+void countStep(int x, int y, int step, int data[N][N], int route[N][N]);
 
-int countStep(int x, int y, int step, int data[N][N], int route[N][N]) {
-    if (checkAndMemo(x - 1, y, step + 1, data, route)) {
-        countStep(x - 1, y, step + 1, data, route);
-    }
-    if (checkAndMemo(x + 1, y, step + 1, data, route)) {
-        countStep(x + 1, y, step + 1, data, route);
-    }
-
-    if (checkAndMemo(x, y + 1, step + 1, data, route)) {
-        countStep(x, y + 1, step + 1, data, route);
-    }
-    if (checkAndMemo(x, y + 1, step + 1, data, route)) {
-        countStep(x, y + 1, step + 1, data, route);
+void checkAndMemo(int x, int y, int step, int data[N][N], int route[N][N]) {
+    if (!((x < N && x >= 0) && (y < N && y >= 0))) return;
+    if ((route[x][y] == 0 || route[x][y] > step) && (data[x][y] == 0 || data[x][y] == -2)) {
+        route[x][y] = step;
+        countStep(x, y, step + 1, data, route);
     }
 }
 
-int trace_check(int x, int y, int route[N][N], int step) {
-    if ((x < N && x >= 0) && (y < N && y >= 0)) {
-        if (route[x][y] == step) return 1;
-    }
-    return 0;
+void countStep(int x, int y, int step, int data[N][N], int route[N][N]) {
+    checkAndMemo(x - 1, y, step + 1, data, route);
+    checkAndMemo(x + 1, y, step + 1, data, route);
+    checkAndMemo(x, y - 1, step + 1, data, route);
+    checkAndMemo(x, y + 1, step + 1, data, route);
 }
 
-int trace(int data[N][N], int route[N][N], int step, int x, int y) {
-    if (trace_check(x - 1, y, route, step - 1)) {
+void trace(int data[N][N], int route[N][N], int step, int x, int y);
 
-        data[x - 1][y] = 1;
+int trace_check(int data[N][N], int x, int y, int route[N][N], int step) {
+    if (x < N && x >= 0 && y < N && y >= 0 && route[x][y] == step) {
+        data[x][y] = 1;
         if (step > 1) {
             trace(data, route, step - 1, x - 1, y);
         }
-        return 0;
+        return 1;
     }
+    return 0;
+}
 
-    if (trace_check(x + 1, y, route, step - 1)) {
-        data[x + 1][y] = 1;
-        if (step > 1) {
-            trace(data, route, step - 1, x + 1, y);
-        }
-        return 0;
-    }
-
-    if (trace_check(x, y + 1, route, step - 1)) {
-        data[x][y + 1] = 1;
-        if (step > 1) {
-            trace(data, route, step - 1, x, y + 1);
-        }
-        return 0;
-    }
-
-    if (trace_check(x, y - 1, route, step - 1)) {
-        data[x][y - 1] = 1;
-        if (step > 1) {
-            trace(data, route, step - 1, x, y - 1);
-        }
-        return 0;
-    }
+void trace(int data[N][N], int route[N][N], int step, int x, int y) {
+    if (trace_check(data, x + 1, y, route, step - 1)) return;
+    if (trace_check(data, x - 1, y, route, step - 1)) return;
+    if (trace_check(data, x, y + 1, route, step - 1)) return;
+    if (trace_check(data, x, y - 1, route, step - 1)) return;
 }
 
 int process(int data[N][N], point start, point goal) {
     int route[N][N] = {{0,},};
-    countStep(1, 1, 1, data, route);
-    printf("%d", route[goal.x][goal.y]);
+    countStep(start.x, start.y, 1, data, route);
+    printf("ステップ数: %d", route[goal.x][goal.y]);
     trace(data, route, route[goal.x][goal.y], goal.x, goal.y);
     for (int i = 0; i < 10; ++i) {
         for (int j = 0; j < 10; ++j) {
